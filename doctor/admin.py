5cf462df07d5fa8bde7a5e005fed9e96ffbec900
Roshan G <roshan.g@codingmart.com>
2020-03-04 14:40:28 +0530

    Added new feedback model

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from doctor.models import Feedback
 # Register your models here.
+admin.site.register(Feedback)
--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 from app.models import BaseModel
+from users.models import UserProfile
+from appointment.models import Appointment
 # Create your models here.
+
+
+class Feedback(models.Model):
+    """Model definition for Feedback."""
+    rating = models.IntegerField()
+    doctor = models.ForeignKey(to=UserProfile, limit_choices_to={
+        'user_type': 'doctor'
+    }, on_delete=models.CASCADE, related_name='doctor_feedback')
+    patient = models.ForeignKey(to=UserProfile, limit_choices_to={
+        'user_type': 'patient'
+    }, on_delete=models.CASCADE, related_name='patient_feedback')
+    appointment = models.ForeignKey(to=Appointment, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    class Meta:
+        """Meta definition for Feedback."""
+
+        verbose_name = 'Feedback'
+        verbose_name_plural = 'Feedbacks'
+
+    def __str__(self):
+        """Unicode representation of Feedback."""
+        return f'{self.id} - Doctor={self.doctor.first_name} Patient={self.patient.first_name} Rating={self.rating}'
